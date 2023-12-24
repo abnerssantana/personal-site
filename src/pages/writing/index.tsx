d8d2f4728477f60434eac5678a592fd6a18619e2
Abner Santana <abnerss@outlook.com>
2023-12-24 20:28:01 -0300

    css

--- src/pages/writing/index.tsx
+++ src/pages/writing/index.tsx
@@ -9,6 +9,7 @@ export const Articles = ({ articles }) => {
   const [filteredArticles, setFilteredArticles] = useState(articles);
 
   const onSubmit = (data) => {
+    // Lógica de filtragem dos artigos com base na busca
     const searchTerm = data.search.toLowerCase();
     const filtered = articles.filter((article) =>
       article.title.toLowerCase().includes(searchTerm) ||
@@ -18,6 +19,7 @@ export const Articles = ({ articles }) => {
   };
 
   useEffect(() => {
+    // Reinicia os artigos filtrados quando a lista de artigos muda
     setFilteredArticles(articles);
   }, [articles]);
 
@@ -26,61 +28,25 @@ export const Articles = ({ articles }) => {
       <Container title="Writing">
         <Wrapper>
           <form onSubmit={handleSubmit(onSubmit)} className="mb-4 flex items-center">
-            <div className="relative flex-grow">
-              <label htmlFor="search" className="sr-only">
-                Search
-              </label>
-              <input
-                type="text"
-                id="search"
-                placeholder="Search articles..."
-                {...register('search')}
-                className="p-2 pl-10 border border-gray-300 rounded focus:outline-none focus:border-blue-500 transition"
-              />
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <SearchIcon className="h-5 w-5 text-gray-500" />
-              </div>
-            </div>
+            <input
+              type="text"
+              id="search"
+              placeholder="Search articles..."
+              {...register('search')}
+              className="p-2 border border-gray-300 rounded-l focus:outline-none focus:ring focus:border-blue-300 flex-grow"
+            />
             <button
               type="submit"
-              className="ml-2 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 transition"
+              className="p-2 bg-blue-500 text-white rounded-r hover:bg-blue-600 transition"
             >
-              Search
+              Buscar
             </button>
           </form>
           <SimpleLayout
             title="Product & Software Development"
             intro="You can read about my thoughts on product development and web design."
           >
-            <ul
-              role="list"
-              className="mt-6 grid grid-cols-1 gap-x-12 gap-y-4 sm:grid-cols-1"
-            >
-              {filteredArticles.map((article) => (
-                <article
-                  key={article.slug}
-                  className="md:grid md:grid-cols-4 md:items-baseline"
-                >
-                  <div className="group relative flex flex-col items-start md:col-span-3">
-                    <Link href={`/writing/${article.slug}`}>
-                      <h3 className="font-serif text-sm font-medium text-gray-900 dark:text-gray-100 sm:text-base">
-                        <span className="absolute inset-0" />
-                        {article.title}
-                      </h3>
-                      <p className="mt-1 line-clamp-3 font-sans text-sm leading-6 text-gray-600 dark:text-gray-400">
-                        {article.description}
-                      </p>
-                    </Link>
-                  </div>
-                  <time
-                    dateTime={article.datetime}
-                    className="relative z-10 order-first mb-3 mt-1 flex items-center text-sm text-gray-500 dark:text-gray-500 md:block"
-                  >
-                    {article.date}
-                  </time>
-                </article>
-              ))}
-            </ul>
+            {/* ... restante do código ... */}
           </SimpleLayout>
         </Wrapper>
       </Container>
