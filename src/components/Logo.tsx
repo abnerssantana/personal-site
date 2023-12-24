792349d8496430e1a9a706b46fb99b9d81a0df7a
Abner Santana <abnerss@outlook.com>
2023-12-24 19:12:52 -0300

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,1 @@
-export const Logo = (props) => {
-  return (
-    <h2 class="text-xl font-black text-gray-600 dark:text-gray-400 sm:text-2xl">Abner Santana</h2>
-  )
-}
+<h2 class="text-xl font-black text-gray-600 dark:text-gray-400 sm:text-2xl">Abner Santana</h2>
