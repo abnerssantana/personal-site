06e469878d91e76914f8ff67322b664535ca0cff
Abner Santana <abnerss@outlook.com>
2023-12-24 21:36:29 -0300

    test css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
       <Container>
         <Wrapper>
           <div className="mx-auto pb-24 pt-24 sm:pb-40 sm:pt-40">
-            <div className="text-center">
+            <div className="text-center pb-24 pt-24 sm:pb-40 sm:pt-40">
               <h2 className="text-xl font-normal text-gray-600 dark:text-gray-400 sm:text-2xl">
               TECH - NERD - MARATHONER - YOUTUBER
               </h2>
