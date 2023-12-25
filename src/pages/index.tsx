9c88c8704d507f3897a2c2f97b6608564168128b
Abner Santana <abnerss@outlook.com>
2023-12-25 11:39:14 -0300

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,17 @@ export const Home = () => {
         <Wrapper>
           <div className="mx-auto pb-24 pt-24 md:pb-36 md:pt-36">
             <div className="text-center">
-              <h2 className="text-2xl font-normal text-gray-600 dark:text-gray-400 md:text-lg">
+              <h2 className="text-3xl font-normal text-gray-600 dark:text-gray-400 md:text-lg">
                 TECH - NERD - MARATHONER - YOUTUBER
               </h2>
               <h1
                 className={clsx(
-                  'font-sans text-6xl uppercase font-black leading-tight text-gray-900 dark:text-gray-100 md:text-5xl md:leading-snug'
+                  'font-sans text-8xl uppercase font-black leading-tight text-gray-900 dark:text-gray-100 md:text-5xl md:leading-snug'
                 )}
               >
                 VIVENDO A CORRIDA
               </h1>
-              <p className="text-base leading-8 text-gray-600 dark:text-gray-400">
+              <p className="text-lg leading-8 text-gray-600 dark:text-gray-400">
                 faça menos, faça melhor, faça uma única vez e faça no lugar certo
               </p>
               <div className="mt-8 flex justify-center gap-x-4">
