2d5aa9293b2339d7a39bbcda3719b73f09df9573
Abner Santana <abnerss@outlook.com>
2023-12-26 14:28:09 -0300

    css videos

--- src/lib/LatestVideos.js
+++ src/lib/LatestVideos.js
@@ -12,6 +12,7 @@ const LatestVideos = () => {
           part: 'snippet',
           channelId: 'UCKl5xev9VFkTV0YTHO74DIQ',
           maxResults: 4,
+          type: 'video',
           order: 'date',
           key: 'AIzaSyDZ4X2oLeEYyAkQvOxtIET-terp-SZWonk',
         },
@@ -27,7 +28,7 @@ const LatestVideos = () => {
   }, []);
 
   return (
-    <div className="mt-6 grid grid-cols-1 gap-x-12 gap-y-4 sm:grid-cols-1">
+    <div className="mt-6 grid grid-cols-1 gap-x-12 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
       {latestVideos.map((video) => (
         <div key={video.id.videoId} className="flex flex-col items-center">
           <a
