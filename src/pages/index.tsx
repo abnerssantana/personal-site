4a3620edf372720e935b092e75829f2eaf1b72f5
Abner Santana <abnerss@outlook.com>
2023-12-26 14:18:00 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 import { Container, Wrapper } from 'src/components/'
-import { LatestVideos } from 'src/lib/LatestVideos';
+import LatestVideos from 'src/lib/LatestVideos';
 
 export const Home = () => {
   return (
