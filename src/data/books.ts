6738e988ece17f83bb17c771d4c127e7e38805c8
Abner Santana <abnerss@outlook.com>
2023-12-26 11:01:15 -0300

    jack daniels book

--- src/data/books.ts
+++ src/data/books.ts
@@ -8,6 +8,14 @@ interface Book {
 
 export const books: Book[] = [
   {
+    title: 'Fórmula de Corrida de Daniels',
+    author: 'Jack Daniels',
+    cover: 'static/books/JackDaniels.jpg',
+    amazonUrl: 'https://www.amazon.com.br/Fórmula-Corrida-Daniels-Jack/dp/8565852032',
+    review:
+    "Me sinto como um discípulo de Jack Daniels, de tanto que prego sobre seus ensinos, para mim o melhor e mais completo livro sobre treinamento de corrida, para corredores dos 800m até a maratona.Me sinto como um discípulo de Jack Daniels, de tanto que prego sobre seus ensinos, para mim o melhor e mais completo livro sobre treinamento de corrida, para corredores dos 800m até a maratona.",
+  }, 
+  {
     title: 'Ester na Casa da Pérsia: e a Vida Cristã no Exílio Secular',
     author: 'Emilio Garofalo Neto',
     cover: 'static/books/ester.jpg',
