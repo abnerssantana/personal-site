315451f373424affe45922b658cbbc1700067320
Abner Santana <abnerss@outlook.com>
2023-12-30 20:43:56 -0300

    layout

--- src/pages/tv.tsx
+++ src/pages/tv.tsx
@@ -11,44 +11,56 @@ export const Card = ({ title, description, url, img, category }: Show) => {
   const { hostname } = new URL(url)
 
   return (
-    <Link href={url} target="_blank" rel="noopener noreferrer">
-      <div className="group relative h-full rounded-lg border border-gray-200 border-transparent transition-colors hover:border-gray-400 dark:border-gray-800 dark:hover:border-gray-500 ">
-        <div className="relative overflow-hidden rounded-lg p-6">
-          <div className="flex w-full items-center justify-between">
-            <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4 w-32 sm:col-span-2 sm:w-40">
-              {img ? (
-                <Image
-                  quality={100}
-                  width={160}
-                  height={140}
-                  src={img}
-                  alt={title}
-                  className="object-cover"
-                />
-              ) : null}
-            </div>
-            <span
-              className={clsx(
-                'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium capitalize',
-                'border-gray-300 bg-gray-100 text-gray-800 dark:border-gray-800 dark:bg-gray-500/10 dark:text-gray-300'
-              )}
-            >
-              {category}
-            </span>
+
+    <ul
+      role="list"
+      className="mt-8 grid grid-cols-1 divide-y divide-gray-200 dark:divide-gray-800">
+
+      <li key={book.title} className="flex py-8">
+        <div className="space-y-4 sm:grid sm:grid-cols-8 sm:items-start sm:gap-4 sm:space-y-0">
+          <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4 w-32 sm:col-span-2 sm:w-40">
+            {img ? (
+              <Image
+                quality={100}
+                width={160}
+                height={140}
+                src={img}
+                alt={title}
+                className="object-cover"
+              />
+            ) : null}
           </div>
-          <h2 className="mt-4 text-base font-bold text-gray-900 dark:text-gray-50">
-            {title}
-          </h2>
-          <p className="mt-1 text-sm text-gray-700 dark:text-gray-400">
-            {description}
-          </p>
-          <p className="relative z-10 mt-6 flex text-sm font-medium text-gray-700 transition dark:text-gray-500">
-            <LinkIcon className="h-5 w-5 flex-none" />
-            <span className="ml-2">{hostname}</span>
-          </p>
+          <div className="sm:col-span-6 sm:ml-2">
+            <div className="space-y-3">
+              <div className="space-y-1">
+                <h3 className="text-lg font-bold text-gray-900 dark:text-gray-50 sm:text-xl">
+                  {title}
+                </h3>
+                <p className="text-sm font-medium text-gray-500 dark:text-gray-500">
+                  Autor: {category}
+                </p>
+              </div>
+              <p className="dark:text-gray-4000 text-justify font-sans text-sm text-gray-600 dark:text-gray-400">
+                {description}
+              </p>
+
+              <div className="space-x-3">
+                <Button
+                  href={url}
+                  variant="outline"
+                  target="_blank"
+                >
+                  Review
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </Link>
+      </li>
+    </ul>
+
+
+
   )
 }
 
