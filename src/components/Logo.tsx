ebe8b1d4d2427053af52fb267a73043fdbf46bb7
Abner Santana <abnerss@outlook.com>
2023-12-31 20:51:43 -0300

    logo fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,12 @@
 export const Logo = (props) => {
   return (
-
     <svg fill="currentColor" aria-hidden="true" {...props} viewBox="0 0 24 24" width="22.000000pt" height="22.000000pt">
-      <g transform="translate(0.000000,22.000000) scale(0.100000,-0.100000)"
-        fill="#000000" stroke="none">
+      <g transform="translate(0.000000,22.000000) scale(0.100000,-0.100000)" stroke="none">
         <path d="M62 204 c-50 -35 -48 -112 4 -174 33 -38 55 -38 88 0 68 82 43 190
 -44 190 -14 0 -36 -7 -48 -16z m33 -125 c4 -11 5 -23 2 -26 -9 -9 -47 17 -47
 32 0 23 38 18 45 -6z m75 6 c0 -15 -38 -41 -47 -32 -3 3 -2 15 2 26 7 24 45
 29 45 6z"/>
       </g>
     </svg>
-
   )
 }
