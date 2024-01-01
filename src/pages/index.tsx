25b5405c35e26944dcdaea90ee65b18b270df89f
Abner Santana <abnerss@outlook.com>
2024-01-01 14:59:11 -0300

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,13 +22,14 @@ export const Home = () => {
               <p className="text-lg leading-8 text-gray-600 dark:text-gray-400">
                 faça menos, faça melhor, faça uma única vez e faça no lugar certo
               </p>
-              <div className="mt-8 flex justify-center gap-x-4">
+              
               <h2 className="text-2xl font-normal text-gray-600 dark:text-gray-400 md:text-lg">
                 Apoiam minha jornada na corrida:
               </h2>
               <p className="text-lg leading-8 text-gray-600 dark:text-gray-400">
               Rafael Cordeiro, Dhyocleyson, Marcus vinicius e Ivan Simei
               </p>
+              <div className="mt-8 flex justify-center gap-x-4">
                 <Link
                   href="https://www.youtube.com/channel/UCKl5xev9VFkTV0YTHO74DIQ/join"
                   className="inline-flex items-center rounded-lg border border-transparent px-4 py-1.5 text-sm font-semibold text-gray-700 transition-colors hover:border-gray-400 dark:text-gray-100 "
