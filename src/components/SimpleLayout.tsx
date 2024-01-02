7b2f75f4f6186e387944e49a77488498cf458d7c
Abner Santana <abnerss@outlook.com>
2024-01-02 14:38:05 -0300

    css

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 export function SimpleLayout({ title, intro, children }) {
   return (
     <>
-      <header className="max-w-2xl">
+      <header className="max-w-full">
         <h1
           className={clsx(
             'text-xl font-bold tracking-wide text-gray-900 dark:text-gray-100 sm:text-2xl'
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,13 +9,13 @@ export const About = () => {
           <div>
             <h1
               className={clsx(
-                'font-serif text-xl font-medium tracking-wide text-gray-900 dark:text-gray-100 sm:text-2xl'
+                'font-sans text-xl font-medium tracking-wide text-gray-900 dark:text-gray-100 sm:text-2xl'
               )}
             >
               E aí, Abner Santana
             </h1>
           </div>
-          <div className="mt-2 space-y-4 text-justify font-serif text-sm text-gray-600 dark:text-gray-400 sm:text-base">
+          <div className="mt-2 space-y-4 text-justify font-sans text-sm text-gray-600 dark:text-gray-400 sm:text-base">
             <p>Desde os 13 anos, venho imerso no universo da computação, trilhando diferentes caminhos que moldaram minha trajetória. Iniciei minha jornada em lojas de informática, onde adquiri conhecimentos sólidos em manutenção e instalações de rede. Com o tempo, direcionei meu foco para a área que verdadeiramente me apaixonou: a infraestrutura e segurança de TI.</p>
             <p>Minha carreira é um constante aprendizado e evolução, enfrentando diariamente desafios que impulsionam minha paixão pela profissão. A capacidade de solucionar problemas complexos e aprimorar a segurança dos sistemas são aspectos que me motivam intensamente.</p>
             <p>Paralelamente, a corrida entrou em minha vida como um catalisador de transformações. Engajado em buscar performance, participei de grandes competições e me tornei um atleta federado. Essa jornada esportiva não é apenas física; é também uma fonte constante de aprendizado sobre treinamento e fisiologia.</p>
@@ -62,13 +62,13 @@ export const About = () => {
             </p>
           </div>
           <div>
-            <h2 className="mt-8 font-serif text-lg font-medium tracking-wide text-gray-900 dark:text-gray-100 sm:text-xl">
+            <h2 className="mt-8 font-sans text-lg font-medium tracking-wide text-gray-900 dark:text-gray-100 sm:text-xl">
               Sobre esse site
             </h2>
-            <p className="mt-2 font-serif text-sm text-gray-600 dark:text-gray-400 sm:text-base">
+            <p className="mt-2 font-sans text-sm text-gray-600 dark:text-gray-400 sm:text-base">
               Caso você esteja se perguntando, este site é:
             </p>
-            <ul className="mt-4 list-inside list-disc pl-4 font-serif text-sm text-gray-600 dark:text-gray-400 sm:text-base">
+            <ul className="mt-4 list-inside list-disc pl-4 font-sans text-sm text-gray-600 dark:text-gray-400 sm:text-base">
               <li>
                 Cuidadosamente codificado à mão com{' '}
                 <TextLink
