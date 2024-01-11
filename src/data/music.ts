e1c7af035053b6426b1c36ec1a56c59e251681eb
Abner Santana <abnerss@outlook.com>
2024-01-11 15:51:33 -0300

    fix

--- src/data/music.ts
+++ src/data/music.ts
@@ -12,8 +12,6 @@ export interface Album {
   title: string
   spotify: string
   deezer: string
-  applemusic: string
-  tidal: string
   category: Genres
   description: string
   img?: string 
@@ -29,36 +27,9 @@ export const albums: Album[] = [
     img: 'static/music/bestbrazil.jpeg',
   },
   {
-    title: 'Blinding Lights',
-    spotify: 'https://www.youtube.com/watch?v=4NRXx6U8ABQ',
-    category: Genres.pop,
-    description: 'A popular pop song by The Weeknd, characterized by its synthwave and 80s-inspired sound.',
-    img: 'static/music/blinding_lights.jpg',
-  },
-  {
-    title: 'Sicko Mode',
-    spotify: 'https://www.youtube.com/watch?v=6ONRf7h3Mdk',
-    category: Genres.hipHop,
-    description: 'A rap song by Travis Scott featuring Drake, known for its unconventional structure and energetic beats.',
-    img: 'static/music/sicko_mode.jpg',
-  },
-  {
-    title: 'Strobe',
-    spotify: 'https://www.youtube.com/watch?v=tKi9Z-f6qX4',
-    category: Genres.electronic,
-    description: 'An electronic music masterpiece by Deadmau5, famous for its progressive house elements and mesmerizing melodies.',
-    img: 'static/music/strobe.jpg',
-  },
-  {
-    title: 'So What',
-    spotify: 'https://www.youtube.com/watch?v=Bchx4oj7VFU',
-    category: Genres.jazz,
-    description: 'A classic jazz composition by Miles Davis, known for its improvisational style and bold trumpet work.',
-    img: 'static/music/so_what.jpg',
-  },
-  {
     title: 'Moonlight Sonata',
-    spotify: 'https://www.youtube.com/watch?v=4Tr0otuiQuU',
+    spotify: 'https://open.spotify.com/playlist/1brTCQ0WLDSf74BaV07duH',
+    deezer: 'https://www.deezer.com/br/playlist/12252277271',
     category: Genres.classical,
     description: 'A timeless classical piece by Beethoven, renowned for its emotional depth and piano virtuosity.',
     img: 'static/music/moonlight_sonata.jpg',
