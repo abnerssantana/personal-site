9c99255b93fe8e7802d9f2480846875c6b5690d9
Abner Santana <abnerss@outlook.com>
2024-01-17 14:14:14 -0300

    css

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -74,7 +74,7 @@ export const Articles = ({ articles }) => {
                   </div>
                   <time
                     dateTime={article.datetime}
-                    className="relative z-10 order-first mb-3 mt-1 flex items-center text-sm text-gray-500 dark:text-gray-500 md:block"
+                    className="relative z-10 order-first mb-3 mt-1 flex items-center text-sm text-gray-500 dark:text-gray-500"
                   >
                     {format(new Date(article.date), 'dd/MM/yyyy')}
                   </time>
