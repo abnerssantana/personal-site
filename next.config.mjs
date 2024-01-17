7a7dfd92c9604e960c7f5ae4d8ac3fe1e502a8a8
Abner Santana <abnerss@outlook.com>
2024-01-17 17:39:37 -0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -50,3 +50,19 @@ export default {
   },
 }
 
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+
+export async function OPTIONS(req: NextRequest) {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(req: NextRequest) {
+  // ...
+
+  return NextResponse.json({ foo: "bar" }, { headers: corsHeaders });
+}
