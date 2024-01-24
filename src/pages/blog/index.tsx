6552bc8cb860ade35b324809492d045652666ad4
Abner Santana <abnerss@outlook.com>
2024-01-24 13:42:35 -0300

    date

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -72,13 +72,13 @@ export const Articles = ({ articles }) => {
                       </p>
                     </Link>
                   </div>
-                  <hr></hr>
                   <time
-                    dateTime={article.datetime}
+                    dateTime={article.date}
                     className="relative z-10 mb-3 mt-1 flex items-center text-sm text-gray-500 dark:text-gray-500 md:block"
                   >
-                    {format(new Date(article.datetime), 'dd/MM/yyyy')}
+                    {format(new Date(article.date), 'dd/MM/yyyy')}
                   </time>
+                  <hr />
                 </article>
               ))}
             </ul>
