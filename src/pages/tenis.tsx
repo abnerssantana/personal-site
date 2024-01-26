35840814c86c44f48b294c3e1ad6e9b1c6340ab8
Abner Santana <abnerss@outlook.com>
2024-01-26 11:19:56 -0300

    css

--- src/pages/tenis.tsx
+++ src/pages/tenis.tsx
@@ -11,7 +11,7 @@ export const TenisCard = ({ title, description, link, img, category }: Shoes) =>
 
     <div
       key={title}
-      className="my-custom-card flex flex-col items-center bg-white dark:bg-gray-800 rounded-lg overflow-hidden border p-4 dark:border-gray-400"
+      className="my-custom-card flex flex-col p-4 border border-gray-200 border-transparent transition-colors hover:border-gray-400 dark:border-gray-800 dark:hover:border-gray-500"
     >
       <div className="grid grid-rows-3 grid-flow-col gap-4">
         <div className="row-span-3"> {img ? (
