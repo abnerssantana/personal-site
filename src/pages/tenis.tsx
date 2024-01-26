36168deb0e5d73f0af4de09244db8c7db33e63cb
Abner Santana <abnerss@outlook.com>
2024-01-26 15:01:11 -0300

    css

--- src/pages/tenis.tsx
+++ src/pages/tenis.tsx
@@ -12,7 +12,6 @@ export const TenisCard = ({ title, description, link, review, img, category, kms
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
             <div key={title} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 {img ? (
@@ -37,7 +36,6 @@ export const TenisCard = ({ title, description, link, review, img, category, kms
                 <p className="text-sm font-medium text-gray-900">Quilômetros: {kms} km</p>
               </div>
             </div>
-          ))}
         </div>
       </div>
     </div>
