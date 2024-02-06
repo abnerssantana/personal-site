30e0c82b552c1c3d9dc37fe4a821daea7a7ae067
Abner Santana <abnerss@outlook.com>
2024-02-06 16:07:13 -0300

    img

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -60,6 +60,9 @@ export const Articles = ({ articles }) => {
                 <article key={article.slug} className="md:grid md:grid-cols-4 md:items-baseline">
                   <div className="group relative flex flex-col items-start md:col-span-7">
                     <Link href={`/blog/${article.slug}`}>
+                      <div className="flex">
+                        <img src={article.image} alt={`Imagem de ${article.title}`} className="w-28 h-28 object-cover rounded-lg" />
+                      </div>
                       <h3 className="text-sm mt-6 font-sans font-bold text-gray-900 dark:text-gray-100 sm:text-lg hover:text-neutral-400">
                         <span className="absolute inset-0" />
                         {article.title}
@@ -68,9 +71,6 @@ export const Articles = ({ articles }) => {
                       <p className="mt-1 mb-2 line-clamp-3 font-sans text-sm leading-6 text-gray-600 dark:text-gray-400">
                         {article.description}
                       </p>
-                      <div className="flex items-center">
-                        <img src={article.image} alt={`Imagem de ${article.title}`} className="w-28 h-28 object-cover rounded-lg" />
-                      </div>
                     </Link>
                   </div>
                 </article>
