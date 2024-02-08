6c8e35e4a4c60565a33c1f919b9e5b00fa1ebb8f
Abner Santana <abnerss@outlook.com>
2024-02-08 16:32:11 -0300

    magic

--- src/pages/magic.tsx
+++ src/pages/magic.tsx
@@ -1,80 +1,100 @@
-import { Container, Wrapper } from 'src/components/'
+import { Container, Wrapper } from 'src/components/';
 
-export const NotFound = () => {
+interface RunData {
+  week: string;
+  type: string;
+  distance: string;
+  duration: string;
+  pace: string;
+  notes: string;
+}
+
+const runs: RunData[] = [
+  {
+    week: 'Monday',
+    type: 'Endurance',
+    distance: '5 mi',
+    duration: '45 min',
+    pace: '9:00/mi',
+    notes: 'Long, steady run',
+  },
+  {
+    week: 'Tuesday',
+    type: 'Speed',
+    distance: '3 mi',
+    duration: '25 min',
+    pace: '8:20/mi',
+    notes: 'Intervals: 400m repeats',
+  },
+  {
+    week: 'Wednesday',
+    type: 'Rest',
+    distance: '-',
+    duration: '-',
+    pace: '-',
+    notes: 'Rest day',
+  },
+  {
+    week: 'Thursday',
+    type: 'Tempo',
+    distance: '4 mi',
+    duration: '32 min',
+    pace: '8:00/mi',
+    notes: 'Comfortably hard run',
+  },
+  {
+    week: 'Friday',
+    type: 'Easy',
+    distance: '3 mi',
+    duration: '28 min',
+    pace: '9:20/mi',
+    notes: 'Recovery run',
+  },
+  {
+    week: 'Saturday',
+    type: 'Long',
+    distance: '8 mi',
+    duration: '70 min',
+    pace: '8:45/mi',
+    notes: 'Weekend long run',
+  },
+  {
+    week: 'Sunday',
+    type: 'Recovery',
+    distance: '3 mi',
+    duration: '30 min',
+    pace: '10:00/mi',
+    notes: 'Easy shakeout',
+  },
+];
+
+export const Magic = () => {
   return (
     <>
-      <Container title="About me">
+      <Container title="Magic Run">
         <Wrapper>
           <div className="mx-auto min-h-full pt-20 pb-20 text-center sm:pt-40 sm:pb-40">
             <div key="1" className="w-full overflow-auto rounded-lg border">
               <table className="w-full text-sm leading-none">
                 <thead>
                   <tr className="border-b last:border-b-0 border-t">
-                    <th className="px-4 py-3 text-left first:rounded-tl-lg">Week</th>
-                    <th className="px-4 py-3 text-left">Type</th>
-                    <th className="px-4 py-3 text-left">Distance</th>
-                    <th className="px-4 py-3 text-left">Duration</th>
-                    <th className="px-4 py-3 text-left">Pace</th>
-                    <th className="px-4 py-3 text-left">Notes</th>
+                    {Object.keys(runs[0]).map((key) => (
+                      <th key={key} className="px-4 py-3 text-left first:rounded-tl-lg">
+                        {key.charAt(0).toUpperCase() + key.slice(1)}
+                      </th>
+                    ))}
                   </tr>
                 </thead>
                 <tbody>
-                  <tr className="border-b last:border-b-0">
-                    <td className="px-4 py-3 text-left font-medium first:rounded-bl-lg">Monday</td>
-                    <td className="px-4 py-3 text-left">Endurance</td>
-                    <td className="px-4 py-3 text-left">5 mi</td>
-                    <td className="px-4 py-3 text-left">45 min</td>
-                    <td className="px-4 py-3 text-left">9:00/mi</td>
-                    <td className="px-4 py-3 text-left">Long, steady run</td>
-                  </tr>
-                  <tr className="border-b last:border-b-0 bg-gray-50">
-                    <td className="px-4 py-3 text-left font-medium">Tuesday</td>
-                    <td className="px-4 py-3 text-left">Speed</td>
-                    <td className="px-4 py-3 text-left">3 mi</td>
-                    <td className="px-4 py-3 text-left">25 min</td>
-                    <td className="px-4 py-3 text-left">8:20/mi</td>
-                    <td className="px-4 py-3 text-left">Intervals: 400m repeats</td>
-                  </tr>
-                  <tr className="border-b last:border-b-0">
-                    <td className="px-4 py-3 text-left font-medium">Wednesday</td>
-                    <td className="px-4 py-3 text-left">Rest</td>
-                    <td className="px-4 py-3 text-left">-</td>
-                    <td className="px-4 py-3 text-left">-</td>
-                    <td className="px-4 py-3 text-left">-</td>
-                    <td className="px-4 py-3 text-left">Rest day</td>
-                  </tr>
-                  <tr className="border-b last:border-b-0 bg-gray-50">
-                    <td className="px-4 py-3 text-left font-medium">Thursday</td>
-                    <td className="px-4 py-3 text-left">Tempo</td>
-                    <td className="px-4 py-3 text-left">4 mi</td>
-                    <td className="px-4 py-3 text-left">32 min</td>
-                    <td className="px-4 py-3 text-left">8:00/mi</td>
-                    <td className="px-4 py-3 text-left">Comfortably hard run</td>
-                  </tr>
-                  <tr className="border-b last:border-b-0">
-                    <td className="px-4 py-3 text-left font-medium">Friday</td>
-                    <td className="px-4 py-3 text-left">Easy</td>
-                    <td className="px-4 py-3 text-left">3 mi</td>
-                    <td className="px-4 py-3 text-left">28 min</td>
-                    <td className="px-4 py-3 text-left">9:20/mi</td>
-                    <td className="px-4 py-3 text-left">Recovery run</td>
-                  </tr>
-                  <tr className="border-b last:border-b-0 bg-gray-50">
-                    <td className="px-4 py-3 text-left font-medium">Saturday</td>
-                    <td className="px-4 py-3 text-left">Long</td>
-                    <td className="px-4 py-3 text-left">8 mi</td>
-                    <td className="px-4 py-3 text-left">70 min</td>
-                    <td className="px-4 py-3 text-left">8:45/mi</td>
-                    <td className="px-4 py-3 text-left">Weekend long run</td>
-                  </tr>
-                  <tr className="border-b last:border-b-0">
-                    <td className="px-4 py-3 text-left font-medium last:rounded-bl-lg">Sunday</td>
-                    <td className="px-4 py-3 text-left">Recovery</td>
-                    <td className="px-4 py-3 text-left">3 mi</td>
-                    <td className="px-4 py-3 text-left">30 min</td>
-                    <td className="px-4 py-3 text-left">10:00/mi</td>
-                    <td className="px-4 py-3 text-left">Easy shakeout</td>
-                  </tr>
+                  {runs.map((run, index) => (
+                    <tr key={index} className={index % 2 === 0 ? '' : 'bg-gray-50'}>
+                      {Object.values(run).map((value, index) => (
+                        <td key={index} className={`px-4 py-3 text-left ${index === 0 ? 'font-medium' : ''}`}>
+                          {value}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
@@ -82,7 +102,7 @@ export const NotFound = () => {
         </Wrapper>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default NotFound
+export default Magic;
